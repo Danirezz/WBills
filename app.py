7eba532a64892b77ab0f0c04e88e32afdd81ea70
JuanCepeda22 <juan.sebastian.cepeda.moreno@gmail.com>
2026-03-09 20:47:52 +0000

    frontend terminado

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"mensaje": "Mi API esta funcionando"}
+templates = Jinja2Templates(directory="templates")
+
+# Servir archivos estáticos
+app.mount("/css", StaticFiles(directory="css"), name="css")
+app.mount("/js", StaticFiles(directory="js"), name="js")
+app.mount("/img", StaticFiles(directory="img"), name="img")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/dashboard", response_class=HTMLResponse)
+def dashboard(request: Request):
+    return templates.TemplateResponse("dashboard.html", {"request": request})
